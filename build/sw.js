6b619b63833a81ba2cba0d44007bc5cea387cf56
shahfahad19 <sfahad4280@gmail.com>
2022-08-20 09:33:31 +0500

    Updated api

--- build/sw.js
+++ build/sw.js
@@ -1,12 +1,12 @@
 // Choose a cache name
-const cacheName = "urdu-news-cache-v3";
+const cacheName = "urdu-news-cache-v2";
 // List the files to precache
 const precacheResources = [
     "/",
     "/images/icons/icon-128x128.png",
     "/images/icons/icon-192x192.png",
     "/images/icons/icon-384x384.png",
-    "/images/icons/icon-512x512.png"
+    "/images/icons/icon-512x512.png",
 ];
 
 // When the service worker is installing, open the cache and add the precache resources to it
